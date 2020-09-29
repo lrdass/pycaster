e9e18c8fbac40cc1eabd0d7f94e900cec12e5d29
lrdass <lsuppes@pm.me>
2020-09-29 00:14:30 -0300

    walking player

--- raycaster.py
+++ raycaster.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 from pygame.locals import *
-import math
+# import math
 
 clock = pygame.time.Clock()
 
@@ -14,7 +14,9 @@ walls = [
     [[300, 100], [300, 300]]
 ]
 
-ray_position = pygame.Vector2(100, 200)
+# ray_position = pygame.Vector2(100, 200)
+
+player = pygame.Vector2(0, 0)
 ray_direction = pygame.Vector2(1, 0)
 
 
@@ -28,10 +30,10 @@ def cast_ray(walls):
         x2 = wall[1][0]
         y2 = wall[1][1]
 
-        x3 = ray_position.x
-        y3 = ray_position.y
-        x4 = (ray_position + ray_direction).x
-        y4 = (ray_position + ray_direction).y
+        x3 = player.x
+        y3 = player.y
+        x4 = (player + ray_direction).x
+        y4 = (player + ray_direction).y
 
         den = (x1-x2) * (y3-y4) - (y1-y2) * (x3-x4)
 
@@ -52,19 +54,19 @@ def cast_ray(walls):
 while True:
 
     screen.fill((0, 0, 0))
-
     mouse = pygame.mouse.get_pos()
     # pygame.draw.line(screen, (255, 255, 255),
     #  (WINDOW[0]/2, WINDOW[1]/2), mouse, 1)
 
-    ray_direction.update((mouse[0] - ray_position.x) - ray_direction.x,
-                         (mouse[1] - ray_position.y) - ray_direction.y)
+    ray_direction.update((mouse[0] - player.x) - ray_direction.x,
+                         (mouse[1] - player.y) - ray_direction.y)
     ray_direction = ray_direction.normalize()
 
     ray_distance = pygame.Vector2(
-        ray_position.x + ray_direction.x*20, ray_position.y+ray_direction.y*20)
+        player.x + ray_direction.x*20, player.y+ray_direction.y*20
+    )
 
-    pygame.draw.line(screen, (255, 255, 0), ray_position, ray_distance)
+    pygame.draw.line(screen, (255, 255, 0), player, ray_distance)
 
     for wall in walls:
         pygame.draw.line(screen, (255, 255, 0), (300, 100), (300, 300))
@@ -77,7 +79,16 @@ while True:
                                (int(point.x), int(point.y)), 3, 3)
 
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                player.y -= 5
+            if event.key == pygame.K_s:
+                player.y += 5
+            if event.key == pygame.K_d:
+                player.x += 5
+            if event.key == pygame.K_a:
+                player.x -= 5
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
